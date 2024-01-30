429e13e7a794a41ae50d4dae72e6328341f1c432
“LeeSuMin” <tn9330@gmail.com>
2024-01-30 11:34:23 +0900

    UPDATED

--- app.py
+++ app.py
@@ -3,12 +3,16 @@
 import streamlit as st
 import seaborn as sns
 
+@st.cache_data
+def load_data():
+    df = sns.load_dataset('iris')
+    return df
+
 def main():
     st.title("Hello World on Streamlit.io")
     st.write(st.__version__)
-
-    df = sns.load_dataset('iris')
-    st.write(df)
+    iris = load_data()
+    st.table(iris)
 
 if __name__ == "__main__":
     main()
